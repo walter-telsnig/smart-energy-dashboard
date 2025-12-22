4e6cae6c0053f2c42591b092a1dc4d7cd5b93bae
SabrinaMuh <sabrina.muhrer@aon.at>
2025-12-22 14:01:41 +0100

    add tests for get functions

---
+++ tests/integration/test_consumption_db.py
@@ -0,0 +1,27 @@
+from fastapi.testclient import TestClient
+from app.main import create_app
+from datetime import datetime, timedelta
+
+client = TestClient(create_app())
+start = datetime(2025,1,1)
+end = start + timedelta(days=7)
+
+def test_consumption_db_get():
+    resp = client.get(
+        "/api/dataManagment/consumption-db",
+        params={
+            "start": start,
+            "end": end
+        },
+    )
+    assert resp.status_code == 200
+
+    data = resp.json()
+    assert len(data) == 169
+
+def test_consumption_db_get_error():
+    resp = client.get(
+        "/api/dataManagment/consumption-db",
+    )
+
+    assert resp.status_code != 200
---
+++ tests/integration/test_pv-minute_db.py
@@ -0,0 +1,27 @@
+from fastapi.testclient import TestClient
+from app.main import create_app
+from datetime import datetime, timedelta
+
+client = TestClient(create_app())
+start = datetime(2025,1,1)
+end = start + timedelta(days=7)
+
+def test_pv_minute_db_get():
+    resp = client.get(
+        "/api/dataManagment/pv_minute-db",
+        params={
+            "start": start,
+            "end": end
+        },
+    )
+    assert resp.status_code == 200
+
+    data = resp.json()
+    assert len(data) == 673
+
+def test_pv_minute_db_get_error():
+    resp = client.get(
+        "/api/dataManagment/pv_minute-db",
+    )
+
+    assert resp.status_code != 200
