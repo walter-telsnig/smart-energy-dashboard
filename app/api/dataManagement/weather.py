de7ef4ef2c917c0314cbdc20c7bfe02664795a86
SabrinaMuh <sabrina.muhrer@aon.at>
2025-12-23 19:34:47 +0100

    Add add function for weather

--- app/api/dataManagement/weather.py
+++ app/api/dataManagement/weather.py
@@ -21,20 +21,30 @@ class WeatherData(BaseModel):
 
 #TODO: create functions for create, get, update and delete
 @router.post("/add")
-def create_data(data: WeatherData):
+def create_data(datetime: datetime, temp_c: float, cloud_cover_pct: float):
     cursor.execute(
         "INSERT INTO weather (datetime, temp_c, cloud_cover_pct) VALUES (%s,%s, %s)",
-        (data.datetime, data.temp_c, data.cloud_cover_pct)
+        (datetime, temp_c, cloud_cover_pct)
     )
     conn.commit()
     return{"status": "success"}
 
-@router.get("")
+@router.get("/list")
 def get_data(start: datetime, end: datetime):
     cursor.execute(
         "SELECT datetime, temp_c, cloud_cover_pct FROM weather "
         "WHERE datetime >= %s AND datetime <= %s ORDER BY datetime",
         (start, end)
+    )
+    rows = cursor.fetchall()
+    return rows
+
+@router.get("")
+def get_element(date_value: datetime):
+    cursor.execute(
+        "SELECT datetime, temp_c, cloud_cover_pct FROM weather "
+        "WHERE datetime = %s",
+        (date_value,)
     )
     rows = cursor.fetchall()
     return rows
--- ui/pages/10_Weather.py
+++ ui/pages/10_Weather.py
@@ -2,7 +2,8 @@ import os
 import streamlit as st
 import requests
 import pandas as pd
-from datetime import datetime
+import pytz
+from datetime import datetime, timedelta
 
 API_BASE_URL = os.getenv("API_BASE_URL", "http://localhost:8000")
 
@@ -12,6 +13,49 @@ if "token" not in st.session_state or st.session_state["token"] is None:
     st.warning("Please log in to access this page.")
     st.stop()
 st.title(":cloud: Weather - DB Version")
+
+path = f"{API_BASE_URL}/api/dataManagment/weather-db"
+
+def existData(date: datetime):
+    response = requests.get(
+        path,
+        params={
+            "date_value": date
+        }
+    )
+    if response.status_code == 200:
+        if(len(response.json()) > 0):
+            return True
+        else:
+            return False
+    else:
+        return None 
+
+with st.expander("Add Data"):
+    date = st.date_input("Date:")
+    time = st.time_input("Time:", value="00:00", step=timedelta(hours=1))
+    temp_c = st.number_input("Temperature Celsius:")
+    cloud_cover_pct = st.number_input("Cloud Cover in %:")
+    if st.button("Confirm"):
+        timestamp = datetime(date.year, date.month, date.day, time.hour, time.minute, time.second, tzinfo=pytz.UTC)
+        exists = existData(timestamp)
+        if exists is None:
+            st.write("Error")
+        elif not exists:
+            response = requests.post(
+                path+"/add",
+                params={
+                    "datetime": timestamp,
+                    "temp_c": temp_c,
+                    "cloud_cover_pct": cloud_cover_pct
+                }
+            )
+            if response.status_code == 200:
+                st.write("Done")
+            else:
+                st.write("Error: " + response.status_code)
+        else:
+            st.write("Exists already")
 
 now = datetime.now()
 
@@ -25,7 +69,7 @@ start_ts = pd.Timestamp(start, tz="UTC")
 end_ts = pd.Timestamp(end, tz="UTC") + pd.Timedelta(days=1)
 
 response = requests.get(
-    f"{API_BASE_URL}/api/dataManagment/weather-db",
+    path+"/list",
     params={
         "start": start_ts,
         "end": end_ts
@@ -37,20 +81,23 @@ response = requests.get(
 #st.write("Raw response:", response.text)
 preview_amount = st.number_input("preview_amount",value=48)
 
-df = pd.DataFrame(response.json(), columns=["datetime", "temp_c", "cloud_cover_pct"])
-df["datetime"] = pd.to_datetime(df["datetime"], errors="coerce")
+if response.status_code == 200: 
+    df = pd.DataFrame(response.json(), columns=["datetime", "temp_c", "cloud_cover_pct"])
+    df["datetime"] = pd.to_datetime(df["datetime"], errors="coerce")
 
-chart_temp, stats, preview = st.tabs(["Charts", "Stats", "Preview"])
-with chart_temp:
-    st.write("Line Chart - Temperature")
-    st.line_chart(df.set_index("datetime")["temp_c"])
-    st.write("Line Chart - Cloud coverage in %")
-    st.line_chart(df.set_index("datetime")["cloud_cover_pct"])
-with stats:
-    st.dataframe(df.iloc[:,1:].describe())
-with preview:
-    st.write("Number of Results: " + str(len(df.index)))
-    if(preview_amount<=len(df.index)):
-        st.dataframe(df.head(preview_amount))
-    else:
-        st.dataframe(df.head(len(df.index)))
+    chart_temp, stats, preview = st.tabs(["Charts", "Stats", "Preview"])
+    with chart_temp:
+        st.write("Line Chart - Temperature")
+        st.line_chart(df.set_index("datetime")["temp_c"])
+        st.write("Line Chart - Cloud Cover in %")
+        st.line_chart(df.set_index("datetime")["cloud_cover_pct"])
+    with stats:
+        st.dataframe(df.iloc[:,1:].describe())
+    with preview:
+        st.write("Number of Results: " + str(len(df.index)))
+        if(preview_amount<=len(df.index)):
+            st.dataframe(df.head(preview_amount))
+        else:
+            st.dataframe(df.head(len(df.index)))
+else:
+    st.write("No Data")
