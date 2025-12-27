3e70921af32c0e15bf4737a9853a909f2aaec71b
SabrinaMuh <sabrina.muhrer@aon.at>
2025-12-27 12:40:13 +0100

    Add delete function to pv and pv_minute

--- app/api/dataManagement/pv_minute.py
+++ app/api/dataManagement/pv_minute.py
@@ -57,3 +57,13 @@ def get_element(date_value: datetime):
     )
     rows = cursor.fetchall()
     return rows
+
+@router.delete("")
+def delete_element(date_value: datetime):
+    cursor.execute(
+        "DELETE FROM pv_minute "
+        "WHERE datetime = %s",
+        (date_value,)
+    )
+    conn.commit()
+    return{"status": "success"}
