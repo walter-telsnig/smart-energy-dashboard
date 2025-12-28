5af316ba6a0539bc15f25490d77f3744967d3b07
SabrinaMuh <sabrina.muhrer@aon.at>
2025-12-28 15:09:24 +0100

    Fix pytest Server Problem

--- tests/integration/test_consumption-minute_db.py
+++ tests/integration/test_consumption-minute_db.py
@@ -1,19 +1,11 @@
-import pytest
 from fastapi.testclient import TestClient
 from app.main import create_app
-from infra.database import Base, engine
+from infra.models import Consumption_Minute
 from datetime import datetime, timedelta
 
 client = TestClient(create_app())
 start = datetime(2025,1,1)
 end = start + timedelta(days=7)
-
-@pytest.fixture(scope="session", autouse=True)
-def create_test_db():
-    Base.metadata.create_all(bind=engine)
-    #Run test
-    yield
-    Base.metadata.drop_all(bind=engine)
 
 def test_consumption_minute_db_get():
     resp = client.get(
